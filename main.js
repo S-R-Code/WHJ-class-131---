b821b524125ddee2d67e5f3ba13711770d3d83e3
S-R-Code <83865650+S-R-Code@users.noreply.github.com>
2021-11-09 15:45:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 img= "";
+Status = "";
 
 function preload(){
-    img = loadImage('dog_cat.jpg')
+    img = loadImage('dog_cat.jpg');
 }
 function setup() {
-    canvas =  createCanvas(640,400);
+    canvas =  createCanvas(640,370);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 function draw(){
-    image(img, 0,0,640,400);
+    image(img, 0,0,640,370);
     fill("#FF0000");
-    text("Dog", 45,75);
+    text("Dog", 195,75);
     noFill();
     stroke("#FF0000");
     rect(30,60,459,350)
+
+    fill("#FF0000");
+    text("Cat", 420, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320)
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    Status = true;
+    objectDetector.detect(img, gotResult);
+
+}
+
+function gotResult(error,results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
 }
